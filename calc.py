5032f66492b835b9aa8c7a2bd272d49447b56443
morozov <morozov@morozov.morozov>
2025-04-25 22:10:09 +0100

    Промежуточный коммит перед pull

--- calc.py
+++ calc.py
@@ -1,16 +1,20 @@
 def add(x, y):
     return x + y
 
+
 def subtract(x, y):
     return x - y
 
+
 def multiply(x, y):
     return x * y
+
 
 def divide(x, y):
     if y == 0:
         return 'Ошибка: деление на ноль'
     return x / y
+
 
 def main():
     print("Простой калькулятор")
@@ -41,6 +45,8 @@ def main():
     else:
         print("Неверный выбор")
 
+
 if __name__ == "__main__":
     main()
+
 
