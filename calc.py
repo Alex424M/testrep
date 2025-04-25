841b0130ad2ff8b93f7fb836b8167f68c63078a1
morozov <morozov@morozov.morozov>
2025-04-25 22:14:00 +0100

    Обновил калькулятор

--- calc.py
+++ calc.py
@@ -1,52 +1,17 @@
-def add(x, y):
-    return x + y
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+op = input("Операция (+, -, *, /): ")
 
-
-def subtract(x, y):
-    return x - y
-
-
-def multiply(x, y):
-    return x * y
-
-
-def divide(x, y):
-    if y == 0:
-        return 'Ошибка: деление на ноль'
-    return x / y
-
-
-def main():
-    print("Простой калькулятор")
-    print("Выберите операцию:")
-    print("1. Сложение")
-    print("2. Вычитание")
-    print("3. Умножение")
-    print("4. Деление")
-
-    choice = input("Введите номер операции (1/2/3/4): ")
-
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Введите первое число: "))
-            num2 = float(input("Введите второе число: "))
-        except ValueError:
-            print("Ошибка: введено не число.")
-            return
-
-        if choice == '1':
-            print("Результат:", add(num1, num2))
-        elif choice == '2':
-            print("Результат:", subtract(num1, num2))
-        elif choice == '3':
-            print("Результат:", multiply(num1, num2))
-        elif choice == '4':
-            print("Результат:", divide(num1, num2))
+if op == '+':
+    print(a + b)
+elif op == '-':
+    print(a - b)
+elif op == '*':
+    print(a * b)
+elif op == '/':
+    if b != 0:
+        print(a / b)
     else:
-        print("Неверный выбор")
-
-
-if __name__ == "__main__":
-    main()
-
-
+        print("Делить на ноль нельзя!")
+else:
+    print("Неизвестная операция")
